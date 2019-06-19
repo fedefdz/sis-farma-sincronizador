0f1e66aae4e5dcaba1df73ce070fa84a647c1804
Federico Fernández <fede.fernandez.it@gmail.com>
2019-06-18 21:18:19 -0300

    trow int parse

--- Sisfarma.Sincronizador.Unycop.Domain.Core/Sincronizadores/PedidoSincronizador.cs
+++ Sisfarma.Sincronizador.Unycop.Domain.Core/Sincronizadores/PedidoSincronizador.cs
@@ -55,11 +55,20 @@ namespace Sisfarma.Sincronizador.Unycop.Domain.Core.Sincronizadores
             {
                 Task.Delay(5).Wait();
                 _cancellationToken.ThrowIfCancellationRequested();
-                
+
+                try
+                {
+                    int.Parse($"{group.Key.Anio}{group.Key.Albaran}");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($@"No se pudo parsear a int ""{group.Key.Anio}{group.Key.Albaran}""", ex);
+                }
+
                 var linea = 0;
                 var fecha = group.Value.First().Fecha; // a la vuelta preguntamos por > fecha
                 var proveedorPedido = group.Value.First().Proveedor.HasValue ? _farmacia.Proveedores.GetOneOrDefaultById(group.Value.First().Proveedor.Value) : null;
-
+                
                 var recepcion = new FAR.Recepcion
                 {
                     Id = int.Parse($"{group.Key.Anio}{group.Key.Albaran}"),
